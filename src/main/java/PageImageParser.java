8fedef4fecb03cdd260b1bcaf52a4f83ad80fad4
Miles <mileshenrichs21@gmail.com>
2019-01-07 22:17:10 -0600

    initial commit

---
+++ src/main/java/PageImageParser.java
@@ -0,0 +1,13 @@
+public class PageImageParser {
+    private final String PAGE_IMAGES_DIR = System.getProperty("user.dir") + "\\src\\main\\resources\\page-images";
+
+    public static void main(String[] args) {
+        PageImageParser parser = new PageImageParser();
+        parser.parseImages();
+    }
+
+    private void parseImages() {
+
+    }
+
+}
