c34703b9e7d2b733aad07b3e9379b83d50634bb0
Miles <mileshenrichs21@gmail.com>
2019-01-10 21:55:15 -0600

    add full story depth-first search data file, remove Google Cloud API dependency

--- src/main/java/GraphBuilder.java
+++ src/main/java/GraphBuilder.java
@@ -1,9 +1,30 @@
-import java.util.List;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.net.URL;
+import java.util.Scanner;
 
 public class GraphBuilder {
 
     public static void main(String[] args) {
-        List<PageImageParser.PageContent> pageContents = PageImageParser.getAllPageContents();
+        GraphBuilder graphBuilder = new GraphBuilder();
+        String graphFileContents = graphBuilder.loadGraphFileContents();
+    }
+
+    private String loadGraphFileContents() {
+        ClassLoader classLoader = this.getClass().getClassLoader();
+        URL resource = classLoader.getResource("story-graph.txt");
+        if(resource != null) {
+            try {
+                File file = new File(resource.getFile());
+                Scanner scanner = new Scanner(file);
+                String text = scanner.useDelimiter("\\A").next();
+                scanner.close();
+                return text;
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
+        return null;
     }
 
 }
